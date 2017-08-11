6517f6f1baf8f9f3fd543fb972e0da0fb7bdd128
lokiverloren <l0k1@null.net>
2017-08-11 22:16:01 +0300

    completed build to resultant steemd

--- build.sh
+++ build.sh
@@ -1,15 +1,69 @@
 #!/bin/bash
 
-echo "Installing debootstrap"
-sudo apt install debootstrap
+echo "Installing pbuilder"
+sudo apt install debootstrap pbuilder devscripts
 
-echo "creating ubuntu 14.04 base image"
-sudo debootstrap --variant=buildd --arch=amd64 trusty ubuntu14
+echo "Creating ubuntu 14.04 base image"
+if [ ! -f ubuntu14.tgz ]; then
+  mkdir ubuntu14
+  sudo pbuilder --create \
+    --distribution trusty \
+    --architecture amd64 \
+    --basetgz ubuntu14.tgz \
+    --debootstrapopts \
+    --variant=buildd
+fi
+
+if [ ! -d ubuntu14 ]; then
+  echo "Unpacking base build image"
+  mkdir ubuntu14
+  cd ubuntu14
+  tar xvf ../ubuntu14.tgz
+  cd ..
+fi
+
+echo "mounting system folders"
+sudo mount -o bind /dev ubuntu14/dev
+sudo mount -o bind /dev/pts ubuntu14/dev/pts
+sudo mount -o bind /sys ubuntu14/sys
+sudo mount -o bind /proc ubuntu14/proc
+cp /etc/resolv.conf ubuntu14/etc/resolv.conf
+
+echo "downloading cmake 3"
+if [ ! -f cmake-3.2.2.tar.gz ]; then
+  wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
+fi
+cp cmake-3.2.2.tar.gz ubuntu14/
+
+echo "downloading boost 1.60"
+if [ ! -f boost_1_60_0.tar.bz2 ]; then
+  URL='http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download'
+  wget -c "$URL" -O boost_1_60_0.tar.bz2
+  [ $( sha256sum boost_1_60_0.tar.bz2 | cut -d ' ' -f 1 ) == \
+    "686affff989ac2488f79a97b9479efb9f2abae035b5ed4d8226de6857933fd3b" ] \
+    || ( echo 'Corrupt download' ; exit 1 )
+fi 
+cp boost_1_60_0.tar.bz2 ubuntu14/
+
+if [ ! -d calibrd ]; then
+  echo "Cloning calibrd Git repository"
+  git clone https://github.com/calibrae-project/calibrd.git
+  cd calibrd
+
+  echo "Updating submodules"
+  git submodule update --init --recursive
+  cd ..
+fi
+
+cp -rf calibrd ubuntu14/
 
 echo "Starting chrooted build script"
 sudo cp buildcalibrd.sh ubuntu14/
-sudo chroot ubuntu14 bash -i
+sudo chroot ubuntu14 bash /buildcalibrd.sh
 
-echo "removing work folder"
-cd ..
+echo "cleaning up"
+sudo umount ubuntu14/dev/pts
+sudo umount ubuntu14/dev
+sudo umount ubuntu14/proc
+sudo umount ubuntu14/sys
 sudo rm -rf ubuntu14
--- buildcalibrd.sh
+++ buildcalibrd.sh
@@ -1,6 +1,52 @@
 #!/bin/bash
-echo "Entered Ubuntu 14 chroot"
-mount none -t proc /proc
-mount none -t sysfs /sys
-mount none -t devpts /dev/pts 
-mount none -t sys /sys
+echo "Installing prerequisite binary packages"
+apt install -y \
+  autoconf \
+  g++ \
+  git \
+  libssl-dev \
+  libtool \
+  make \
+  pkg-config \
+  doxygen \
+  libncurses5-dev \
+  libreadline-dev \
+  libbz2-dev \
+  python-dev \
+  perl \
+  python3 \
+  python3-jinja2 \
+  wget \
+  build-essential \
+  automake \
+  autotools-dev
+
+echo "Building and installing Cmake 3"
+tar xf /cmake-3.2.2.tar.gz
+cd cmake-3.2.2
+./configure
+make -j$(nproc)
+make install
+
+echo "Building and installing Boost 1.60"
+export BOOST_ROOT=$HOME/opt/boost_1_60_0
+tar xjf /boost_1_60_0.tar.bz2
+cd boost_1_60_0
+./bootstrap.sh "--prefix=$BOOST_ROOT"
+./b2 install
+
+mkdir build && cd build
+
+cd /calibrd
+mkdir build
+cd build
+
+echo "Running Cmake autoconfiguration"
+cmake -DCMAKE_BUILD_TYPE=Release ..
+
+echo "building steemd (soon to be renamed calibrd)"
+make -j$(nproc) steemd
+
+bash -i
+# echo "Building cli_wallet"
+# make -j$(nproc) cli_wallet
