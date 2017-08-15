852d27251e42b4b0f403e5e100071cf2852fbdf6
Loki Verloren <louki.sumirniy.stalker@gmail.com>
2017-08-15 11:27:11 +0300

    fixing paths for work folder

--- build.sh
+++ build.sh
@@ -1,90 +1,100 @@
 #!/bin/bash
-
+WORKROOT="/tmp/calibrd-work"
 cd /tmp
 if [ ! -d calibrd-work ]; then
   mkdir calibrd-work
 fi
-cd /tmp/calibrd-work
+cd $WORKROOT
 
-if [ ! -d calibrd ]; then
+if [ ! -d $WORKROOT/calibrd ]; then
 
   echo "Cloning calibrd Git repository"
+  sleep 5
   git clone https://github.com/calibrae-project/calibrd.git
   echo "Entering repository"
-  cd calibrd
+  cd $WORKROOT/calibrd
 
   echo "Updating submodules"
   git submodule update --init --recursive
 
 fi
 
-cd /tmp/calibrd-work
+cd $WORKROOT
 
 echo "Installing pbuilder"
+sleep 5
 sudo apt install -y debootstrap pbuilder devscripts
 
 echo "Creating ubuntu 14.04 base image"
-if [ ! -f ubuntu14.tgz ]; then
+sleep 5
+if [ ! -f $WORKROOT/ubuntu14.tgz ]; then
   sudo pbuilder --create \
     --distribution trusty \
     --architecture amd64 \
-    --basetgz ubuntu14.tgz \
+    --basetgz $WORKROOT/ubuntu14.tgz \
     --debootstrapopts \
     --variant=buildd
 fi
 
-if [ ! -d ubuntu14 ]; then
+if [ ! -d $WORKROOT/ubuntu14 ]; then
   echo "Unpacking base build image"
-  mkdir ubuntu14
-  cd ubuntu14
+  sleep 5
+  mkdir $WORKROOT/ubuntu14
+  cd $WORKROOT/ubuntu14
   tar xvf ../ubuntu14.tgz
   cd ..
 fi
 
-cd /tmp/calibrd-work
-echo "downloading cmake 3"
+cd $WORKROOT
 if [ ! -f cmake-3.2.2.tar.gz ]; then
+  echo "downloading cmake 3"
+  sleep 5
   wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
 fi
-cp cmake-3.2.2.tar.gz ubuntu14/
+cp cmake-3.2.2.tar.gz $WORKROOT/ubuntu14/
 
-cd /tmp/calibrd-work
-echo "Downloading AppImageKit"
-wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
-chmod a+x appimagetool-x86_64.AppImage
-cp appimagetool-x86_64.AppImage ubuntu14/
+cd $WORKROOT
+if [ ! -f $WORKROOT/appimagetool-x86_64.AppImage ]; then
+  echo "Downloading AppImageKit"
+  sleep 5
+  wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+  chmod a+x appimagetool-x86_64.AppImage
+fi
+cp appimagetool-x86_64.AppImage $WORKROOT/ubuntu14/
 
 echo "copying AppDir skeleton to chroot"
-cp -rf calibrd.AppDir ubuntu14/
+sleep 5
+cp -rf $WORKROOT/calibrd.AppDir $WORKROOT/ubuntu14/
 
-echo "downloading boost 1.60"
-if [ ! -f boost_1_60_0.tar.bz2 ]; then
+if [ ! -f $WORKROOT/boost_1_60_0.tar.bz2 ]; then
+  echo "downloading boost 1.60"
+  sleep 5
   URL='http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download'
-  wget -c "$URL" -O boost_1_60_0.tar.bz2
+  wget -c "$URL" -O $WORKROOT/boost_1_60_0.tar.bz2
   [ $( sha256sum boost_1_60_0.tar.bz2 | cut -d ' ' -f 1 ) == \
     "686affff989ac2488f79a97b9479efb9f2abae035b5ed4d8226de6857933fd3b" ] \
     || ( echo 'Corrupt download' ; exit 1 )
 fi 
-cp boost_1_60_0.tar.bz2 ubuntu14/
+cp $WORKROOT/boost_1_60_0.tar.bz2 $WORKROOT/ubuntu14/
 
-cp -rf calibrd ubuntu14/
+cp -rf $WORKROOT/calibrd $WORKROOT/ubuntu14/
 
-cd /tmp/calibrd-work
+cd $WORKROOT
 
 echo "Mounting system folders inside chroot"
-sudo mount -o bind /dev ubuntu14/dev
-sudo mount -o bind /dev/pts ubuntu14/dev/pts
-sudo mount -o bind /sys ubuntu14/sys
-sudo mount -o bind /proc ubuntu14/proc
-cp /etc/resolv.conf ubuntu14/etc/resolv.conf
+sudo mount -o bind /dev $WORKROOT/ubuntu14/dev
+sudo mount -o bind /dev/pts $WORKROOT/ubuntu14/dev/pts
+sudo mount -o bind /sys $WORKROOT/ubuntu14/sys
+sudo mount -o bind /proc $WORKROOT/ubuntu14/proc
+cp /etc/resolv.conf $WORKROOT/ubuntu14/etc/resolv.conf
 
 echo "Starting chrooted build script"
-sudo cp buildcalibrd.sh ubuntu14/
-sudo chroot ubuntu14 bash /buildcalibrd.sh
+sudo cp $WORKROOT/calibrd/buildcalibrd.sh $WORKROOT/ubuntu14/
+sudo chroot $WORKROOT/ubuntu14 bash /buildcalibrd.sh
 
 echo "cleaning up"
-sudo umount ubuntu14/dev/pts
-sudo umount ubuntu14/dev
-sudo umount ubuntu14/proc
-sudo umount ubuntu14/sys
-sudo rm -rf ubuntu14
+sudo umount $WORKROOT/ubuntu14/dev/pts
+sudo umount $WORKROOT/ubuntu14/dev
+sudo umount $WORKROOT/ubuntu14/proc
+sudo umount $WORKROOT/ubuntu14/sys
+sudo rm -rf $WORKROOT/ubuntu14
--- buildcalibrd.sh
+++ buildcalibrd.sh
@@ -46,8 +46,6 @@ cmake -DCMAKE_BUILD_TYPE=Release ..
 echo "building steemd (soon to be renamed calibrd)"
 make -j$(nproc) steemd
 
-cp 
-
 bash -i
 # echo "Building cli_wallet"
 # make -j$(nproc) cli_wallet
