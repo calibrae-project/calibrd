1fa194657bc89bd1ba6c0170dbeddd97943980b9
Loki Verloren <louki.sumirniy.stalker@gmail.com>
2017-08-16 07:15:30 +0300

    Completely bulletproofed, properly

--- build.sh
+++ build.sh
@@ -84,15 +84,13 @@ fi
 prstat "Entering $WORKROOT..."
 cd $WORKROOT
 
-
-if [[ -f $WORKROOT/ubuntu14.tgz ]]; then
-  if [[ ! -f $WORKROOT/.ubuntu14.tgz ]]; then
+# Check if base image has been created
+if [[ ! -f $WORKROOT/.ubuntu14.tgz ]]; then
+  # Clean up mess if previous attempt was interrupted
+  if [ -f $WORKROOT/ubuntu14.tgz ]; then
     prfalse "Base image creation was interrupted, cleaning up"
     rm -f $WORKROOT/ubuntu14.tgz
   fi
-fi
-
-if [ ! -f $WORKROOT/ubuntu14.tgz ]; then
   prstat "Creating Ubuntu 14.04 base image"
   sudo pbuilder --create \
     --distribution trusty \
@@ -100,121 +98,158 @@ if [ ! -f $WORKROOT/ubuntu14.tgz ]; then
     --basetgz $WORKROOT/ubuntu14.tgz \
     --debootstrapopts \
     --variant=buildd &>/dev/null
+  
+  # Process complete, mark it complete
   touch $WORKROOT/.ubuntu14.tgz
   prtrue "Completed creating image"
 else
   prtrue "Ubuntu 14.04 image was already created"
 fi
 
+# Check if base build image was created
 if [[ ! -f $WORKROOT/ubuntu14/.complete ]]; then
+  # Clean up mess if previous attempt was interrupted
   if [[ -d $WORKROOT/ubuntu14 ]]; then
     prfalse "Base image unpacking was interrupted, cleaning up"
     sudo rm -rf $WORKROOT/ubuntu14
   fi
-fi
 
-if [ ! -d $WORKROOT/ubuntu14 ]; then
   prstat "Unpacking base build image"
   mkdir $WORKROOT/ubuntu14
   cd $WORKROOT/ubuntu14
   sudo tar zxfp ../ubuntu14.tgz
   cd ..
+  
+  # Process complete, mark it complete
   touch $WORKROOT/ubuntu14/.complete
   prtrue "Ubuntu 14.04 base image unpacked successfully"
 else
+  # Process was already completed
   prtrue "Ubuntu 14.04 base image was already unpacked"
 fi
 
+# Check if Cmake was downloaded yet
 if [[ ! -f $WORKROOT/.cmake ]]; then
+  # Download was not completed, clean up
   if [[ -f $WORKROOT/cmake-3.2.2.tar.gz ]]; then
+    # Delete intrerrupted download
     rm -f $WORKROOT/cmake-3.2.2.tar.gz
     prfalse "Removed incomplete download of Cmake 3.2.2"
   fi
-fi
-
-cd $WORKROOT
-if [[ ! -f $WORKROOT/cmake-3.2.2.tar.gz ]]; then
+  
   prstat "Downloading Cmake 3.2.2"
+  cd $WORKROOT
+  # Attempt to download Cmake 3.2.2 source tarball
   if wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz &>/dev/null;  then 
-    touch $WORKROOT/.cmake
-  fi
-fi
-cp cmake-3.2.2.tar.gz $WORKROOT/ubuntu14/
+    cp cmake-3.2.2.tar.gz $WORKROOT/ubuntu14/
 
+    # Process complete, mark it complete
+    touch $WORKROOT/.cmake
+    prtrue "Cmake 3.2.2 was downloaded and copied into chroot"
+  fi
+else
+  # Process was already completed
+  prtrue "Cmake 3.2.2 was already installed and copied into chroot"
+fi
+
+# Check if AppImageKit was downloaded and copied into chroot
+# and that the AppImage folder skeleton was copied over
 if [[ ! -f $WORKROOT/.appimage ]]; then
+  # Clean up mess if previous attempt was interrupted
   if [[ -f $WORKROOT/appimagetool-x86_64.AppImage ]]; then
+    # remove previous incomplete download
     rm -f $WORKROOT/appimagetool-x86_64.AppImage
     prfalse "Removed incomplete download of AppImage tool"
   fi
-fi
+  # Clean up previous AppImage skeleton copy if it was interrupted
+  if [[ ! -d $WORKROOT/calibrd/calibrd.AppDir ]]; then
+    # Remove previous incomplete folder
+    rm -rf $WORKROOT/calibrd/calibrd.AppDir
+    prtrue "Removed improperly copied AppDir skeleton"
+  fi
 
-cd $WORKROOT
-if [ ! -f $WORKROOT/appimagetool-x86_64.AppImage ]; then
+  # Download AppImageKit
+  cd $WORKROOT
   prstat "Downloading AppImageKit"
   wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" &>/dev/null
+  # Make file executable
   chmod a+x appimagetool-x86_64.AppImage
+  # Copy into chroot
   cp appimagetool-x86_64.AppImage $WORKROOT/ubuntu14/
+
+  # Copy AppDir skeleton into chroot
   prstat "Copying AppDir skeleton to chroot"
   cp -rf $WORKROOT/calibrd/calibrd.AppDir $WORKROOT/ubuntu14/
+
+  # Process completed successfully, mark complete
   touch $WORKROOT/.appimage
   prtrue "Completed download of AppImageKit and copied with AppDir skeleton into chroot"
 else
+  # Process was already completed
   prtrue "AppImageKit already downloaded and copied into chroot"
 fi
 
 if [[ ! -f $WORKROOT/.boost ]]; then
+fi
+
+# Check if boost download was already done
+if [ ! -f $WORKROOT/.boost ]; then
+  # Remove incomplete download if it was started and not finished
   if [[ -f $WORKROOT/boost_1_60_0.tar.bz2 ]]; then
+    # Delete incomplete download
     rm -f $WORKROOT/boost_1_60_0.tar.bz2
     prfalse "Removed incomplete download of Boost source"
   fi
-fi
 
-if [ ! -f $WORKROOT/.boost ]; then
+  # Download Boost
   prstat "Downloading boost 1.60..."
   URL='http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download'
   wget -c "$URL" -O $WORKROOT/boost_1_60_0.tar.bz2 &>/dev/null
+  
+  # Check that download was correct
   [ $( sha256sum boost_1_60_0.tar.bz2 | cut -d ' ' -f 1 ) == \
     "686affff989ac2488f79a97b9479efb9f2abae035b5ed4d8226de6857933fd3b" ] \
     || ( prfalse 'Corrupt download' ; exit 1 )
+
+  # Copy source tarball into chroot
   cp $WORKROOT/boost_1_60_0.tar.bz2 $WORKROOT/ubuntu14/
+  # Mark process complete
   touch $WORKROOT/.boost
   prtrue "Completed download of boost and placed into chroot"
 else
+  # Process was already completed
   prtrue "Already have Boost downloaded and moved into chroot"
 fi 
 
-if [[ ! -f $WORKROOT/.calibrd ]]; then
-  if [[ -f $WORKROOT/boost_1_60_0.tar.bz2 ]]; then
-    rm -f $WORKROOT/boost_1_60_0.tar.bz2
-    prfalse "Removed incomplete download of Boost source"
+# Check if clone of calibrd repository was completed
+if [[ ! -d $WORKROOT/calibrd/.complete ]]; then
+  # If cloning was interrupted, clean it out
+  if [[ ! -f $WORKROOT/calibrd ]]; then
+    prfalse "Cloning was interrupted, removing incomplete folder"
+    rm -rf $WORKROOT/calibrd
   fi
-fi
 
-# If cloning was interrupted, clean it out
-if [[ ! -f $WORKROOT/calibrd/.complete ]]; then
-  prfalse "Cloning was interrupted, removing incomplete folder"
-  rm -rf $WORKROOT/calibrd
-fi
-
-if [[ ! -d $WORKROOT/calibrd ]]; then
   # Clone the calibrd repository
   prstat "Cloning $BINARYNAME Git repository..."
   git clone https://github.com/calibrae-project/calibrd.git &>/dev/null
 
-  prstat "Entering repository"
   cd $WORKROOT/calibrd
-
+  # Update submodules so repo is ready to build
   prstat "Updating submodules"
   git submodule update --init --recursive &>/dev/null
 
+  # Copy repository into chroot
+  cp -rfp $WORKROOT/calibrd $WORKROOT/ubuntu14/
+
   # Task is complete, does not need to be repeated
-  cp -rf $WORKROOT/calibrd $WORKROOT/ubuntu14/
   touch $WORKROOT/calibrd/.complete
   prtrue "Completed cloning repository and copied into chroot"
 else
+  # Process was already completed
   prtrue "$BINARYNAME repository was already cloned and copied into chroot"
 fi
 
+# Bind mount system folders and mark that procedure was started (so it can be cleaned up)
 prstat "Mounting system folders inside chroot"
 MOUNTEDSYSFOLDERS="1"
 sudo mount -o bind /dev $WORKROOT/ubuntu14/dev
@@ -223,12 +258,14 @@ sudo mount -o bind /sys $WORKROOT/ubuntu14/sys
 sudo mount -o bind /proc $WORKROOT/ubuntu14/proc
 sudo cp /etc/resolv.conf $WORKROOT/ubuntu14/etc/resolv.conf
 
+# Copy chroot build script into chroot and run it
 prstat "Starting chrooted build script"
 sudo cp $WORKROOT/calibrd/buildcalibrd.sh $WORKROOT/ubuntu14/
 sudo chroot $WORKROOT/ubuntu14 bash /buildcalibrd.sh
 
-prstat "copying out completed steemd, which will run on any version of ubuntu from 14.04 to 17.04"
-cp $WORKROOT/ubuntu14/calibrd/build/programs/steemd/steemd $OUTPUTDIR/
+# TODO: Create AppImage
+# prstat "Copying out completed steemd, which will run on any version of ubuntu from 14.04 to 17.04"
+# cp $WORKROOT/ubuntu14/calibrd/build/programs/steemd/steemd $OUTPUTDIR/
 
 cleanup
 # The End
--- buildcalibrd.sh
+++ buildcalibrd.sh
@@ -1,55 +1,136 @@
 #!/bin/bash
-echo "Installing prerequisite binary packages"
-sleep 5
-apt install -y \
-  autoconf \
-  g++ \
-  git \
-  libssl-dev \
-  libtool \
-  make \
-  pkg-config \
-  doxygen \
-  libncurses5-dev \
-  libreadline-dev \
-  libbz2-dev \
-  python-dev \
-  perl \
-  python3 \
-  python3-jinja2 \
-  wget \
-  build-essential \
-  automake \
-  autotools-dev
+# Build calibrd inside Ubuntu 14.04 chroot
+BINARYNAME="steemd"
 
-echo "Building and installing Cmake 3"
-sleep 5
-tar xf /cmake-3.2.2.tar.gz
-cd cmake-3.2.2
-./configure
-make -j$(nproc)
-make install
+# Colours for console output
+BLK='\033[0;30m';DGY='\033[1;30';RED='\033[0;31m';LRD='\033[1;31m';GRN='\033[0;32m';LGN='\033[1;32m'
+ORG='\033[0;33m';YLW='\033[1;33m';BLU='\033[0;34m';LBL='\033[1;34m';PRP='\033[0;35m';LPR='\033[1;35m'
+CYN='\033[0;36m';LCY='\033[1;36m';LGY='\033[0;37m';WHT='\033[1;37m';NC='\033[0m'
+PFX="###"
 
-echo "Building and installing Boost 1.60"
-sleep 5
-export BOOST_ROOT=/boost_1_60_0
-cd /
-tar xvjf /boost_1_60_0.tar.bz2
-cd /boost_1_60_0
-./bootstrap.sh "--prefix=$BOOST_ROOT"
-./b2 install
+function prstat {
+  printf "$PFX $1\n"
+}
+function prtrue {
+  printf "$GRN$PFX $1$NC\n"
+}
+function prfalse {
+  printf "$RED$PFX $1$NC\n"
+}
 
-cd /calibrd
-mkdir build
-cd build
+# Check if prerequisite binary packages are installed
+if [[ ! -f /.binpkgs ]]; then
+  # Binary packages were not yet installed
+  prstat "Installing prerequisite binary packages"
+  apt install -y autoconf \
+    g++ git libssl-dev libtool \
+    make pkg-config doxygen \
+    libncurses5-dev libreadline-dev \
+    libbz2-dev python-dev \
+    perl python3 python3-jinja2 \
+    wget build-essential automake \
+    autotools-dev &>/dev/null
+  # Process complete, mark it  complete
+  touch /.binpkgs
+  prtrue "Prerequisite binary packages installed"
+else
+  # Process was already performed
+  prtrue "Prerequiste binary packages already installed"
+fi
 
-echo "Running Cmake autoconfiguration"
-sleep 5
-cmake -DCMAKE_BUILD_TYPE=Release ..
+# Check if Cmake 3.2.2 has been built
+if [[ ! -f /.cmake ]]; then
+  # Cmake 3.2.2 build has not been marked complete
+  if [[ -d /cmake-3.2.2 ]]; then
+    # Cmake 3.2.2 build was started but did not complete
+    prfalse "Cmake 3.2.2 build was interrupted, cleaning up"
+    rm -rf /cmake-3.2.2
+  fi
 
-echo "building steemd (soon to be renamed calibrd)"
-sleep 5
-make -j$(nproc) steemd
+  # Build Cmake 3.2.2
+  prstat "Building and installing Cmake 3.2.2"
+  tar zxf /cmake-3.2.2.tar.gz
+  cd cmake-3.2.2
+  
+  # Configure build
+  prstat "Configuring autotools build"
+  ./configure
+
+  # Build Cmake 3.2.2
+  prstat "Building Cmake 3.2.2"
+  make -j$(nproc)
+
+  # Install Cmake 3.2.2
+  make install
+
+  # Process completed successfully, mark complete
+  touch /.cmake
+  prtrue "Cmake 3.2.2 build complete"
+else
+  # Cmake was already built and installed
+  prtrue "Cmake 3.2.2 was previously built and installed"
+fi
+
+# Check to see if build was built and installed
+if [[ ! -f /.boost ]]; then
+  # Boost build has not been marked complete
+  if [[ -d /boost_1_60_0 ]]; then
+    # Boost build was interrupted
+    prfalse "Boost build was interrupted, cleaning up"
+    rm -rf /boost_1_60_0
+  fi
+
+  # Build and install Boost 1.60
+  prstat "Building and installing Boost 1.60"
+  
+  # Create environment variable to tell build where to find boost
+  export BOOST_ROOT=/boost_1_60_0
+  
+  # Unpack Boost source tarball
+  cd /
+  tar xjf /boost_1_60_0.tar.bz2
+
+  # Start Boost 1.60 Build
+  cd /boost_1_60_0
+  ./bootstrap.sh "--prefix=$BOOST_ROOT"
+  ./b2 install
+
+  # Process completed successfully, mark complete
+  touch /.boost
+  prtrue "Boost build successful"
+else
+  # Boost was already built and installed
+  prtrue "Boost was already built and installed"
+fi
+
+# Check to see if calibrd was already built
+if [[ ! -f /.calibrd]]; then
+  # If process was interrupted, clean up
+  if [[ /calibrd/build ]]; then
+    prfalse "$BINARYNAME build was interrupted, cleaning up"
+    rm -rf /calibrd/build
+  fi
+
+  prstat "Initiating $BINARYNAME build"
+  mkdir /calibrd/build
+  cd /calibrd/build
+
+  # Configure build
+  prstat "Running Cmake autoconfiguration for $BINARYNAME"
+  cmake -DCMAKE_BUILD_TYPE=Release ..
+
+  # Build!
+  prstat "building $BINARYNAME"
+  make -j$(nproc) $BINARYNAME
+
+  # Process completed successfully, mark complete
+  touch /.calibrd
+  prtrue "Completed $BINARYNAME build"
+else
+  # Was already built
+  prtrue "$BINARYNAME was already built"
+fi
+
 #echo "dropping to shell inside chroot so you can test /calibrd/programs/steemd/steemd binary is operational"
 #bash -i
 # echo "Building cli_wallet"
