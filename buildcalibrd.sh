9adbe04e380d1d0fb6c7edae6af413a4cb4640a3
Loki Verloren <l0k1@null.net>
2017-08-19 06:38:11 +0300

    Added licence to header of script

--- buildcalibrd.sh
+++ buildcalibrd.sh
@@ -1,4 +1,46 @@
 #!/bin/bash
+##############################################################################
+#                                                                            #
+#  buildcalibrd.sh                                                           #
+#                                                                            #
+#  This is the script that runs the build inside the chroot for the          #
+#  building of calibrd, the server for the Calibrae distributed              #
+#  decentralised social network system.                                      #
+#                                                                            #
+##############################################################################
+#                                                                            #
+#  Credits for creation of this code can be found in the Git log.            #
+#                                                                            #
+##############################################################################
+#                                                                            #
+#  "LICENCE" for redistribution, derivation and use of this code             #
+#                                                                            #
+#  This is free and unencumbered software released into the public domain.   #
+#                                                                            #
+#  Anyone  is  free  to  copy,  modify,  publish,  use,  compile,  sell, or  #
+#  distribute  this software,  either in source code  form or as a compiled  #
+#  binary, for any purpose, commercial or non-commercial, and by any means.  #
+#                                                                            #
+#  In jurisdictions  that recognize  copyright laws,  the author or authors  #
+#  of this software dedicate any and all copyright interest in the software  #
+#  to the public  domain.   We make this dedication  for the benefit of the  #
+#  public at  large and to  the detriment  of our heirs  and successors. We  #
+#  intend  this  dedication  to  be   an  overt  act  of  relinquishment in  #
+#  perpetuity  of  all present  and  future  rights to this  software under  #
+#  copyright law.                                                            #
+#                                                                            #
+#  THE  SOFTWARE  IS  PROVIDED  "AS IS",  WITHOUT  WARRANTY  OF  ANY  KIND,  #
+#  EXPRESS  OR  IMPLIED,  INCLUDING  BUT NOT LIMITED  TO  THE WARRANTIES OF  #
+#  MERCHANTABILITY,   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  #
+#  IN  NO EVENT  SHALL THE  AUTHORS BE LIABLE  FOR ANY  CLAIM,   DAMAGES OR  #
+#  OTHER  LIABILITY,  WHETHER IN AN ACTION OF CONTRACT,  TORT OR OTHERWISE,  #
+#  ARISING FROM,  OUT OF OR IN CONNECTION WITH  THE SOFTWARE  OR THE USE OR  #
+#  OTHER DEALINGS IN THE SOFTWARE.                                           #
+#                                                                            #
+#  For more information, please refer to <http://unlicense.org/>             #
+#                                                                            #
+##############################################################################
+
 # set locale to standart en_US.UTF-8 in case user uses other locale, to stop filling build output with dumb locale errors
 if [[ ! -f /.locale ]]; then
   locale-gen en_US.UTF-8
@@ -159,7 +201,4 @@ else
   prtrue "$BINARYNAME and cli_wallet were already built"
 fi
 
-#bash -i
-
-# echo "Building cli_wallet"
-# make -j$(nproc) cli_wallet
+# The End
--- enterchroot.sh
+++ enterchroot.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
-# Enter chroot containing built servers/apps for dev testing
+##############################################################################
+#                                                                            #
+#  enterchroot.sh                                                            #
+#                                                                            #
+#  Script to allow the manual configuration, building and testing of         #
+#  calibrd, the server for the Calibrae distributed decentralised social     #
+#  network system                                                            #
+#                                                                            #
+##############################################################################
+#                                                                            #
+#  Credits for creation of this code can be found in the Git log             #
+#                                                                            #
+##############################################################################
+#                                                                            #
+#  "LICENCE" for redistribution, derivation and use of this code             #
+#                                                                            #
+#  This is free and unencumbered software released into the public domain.   #
+#                                                                            #
+#  Anyone  is  free  to  copy,  modify,  publish,  use,  compile,  sell, or  #
+#  distribute  this software,  either in source code  form or as a compiled  #
+#  binary, for any purpose, commercial or non-commercial, and by any means.  #
+#                                                                            #
+#  In jurisdictions  that recognize  copyright laws,  the author or authors  #
+#  of this software dedicate any and all copyright interest in the software  #
+#  to the public  domain.   We make this dedication  for the benefit of the  #
+#  public at  large and to  the detriment  of our heirs  and successors. We  #
+#  intend  this  dedication  to  be   an  overt  act  of  relinquishment in  #
+#  perpetuity  of  all present  and  future  rights to this  software under  #
+#  copyright law.                                                            #
+#                                                                            #
+#  THE  SOFTWARE  IS  PROVIDED  "AS IS",  WITHOUT  WARRANTY  OF  ANY  KIND,  #
+#  EXPRESS  OR  IMPLIED,  INCLUDING  BUT NOT LIMITED  TO  THE WARRANTIES OF  #
+#  MERCHANTABILITY,   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  #
+#  IN  NO EVENT  SHALL THE  AUTHORS BE LIABLE  FOR ANY  CLAIM,   DAMAGES OR  #
+#  OTHER  LIABILITY,  WHETHER IN AN ACTION OF CONTRACT,  TORT OR OTHERWISE,  #
+#  ARISING FROM,  OUT OF OR IN CONNECTION WITH  THE SOFTWARE  OR THE USE OR  #
+#  OTHER DEALINGS IN THE SOFTWARE.                                           #
+#                                                                            #
+#  For more information, please refer to <http://unlicense.org/>             #
+#                                                                            #
+##############################################################################
 
 # This is the hard-coded relative location also in build.sh and both must be changed (or maybe we can fix this later)
 WORKROOT="$HOME/calibrd-work"
@@ -51,3 +91,5 @@ prfalse "You are now in the chroot, have fun!"
 sudo chroot $WORKROOT/ubuntu14 bash -i
 
 cleanup
+
+# The End
